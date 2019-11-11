26a2db3728508ea9dba3347f8aca4cb86e470d97
randomtask1155 <danplynch@gmail.com>
2019-11-11 14:18:07 -0600

    add root ca for godaddy

--- email.go
+++ email.go
@@ -3,7 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/rest"
 	smail "github.com/sendgrid/sendgrid-go/helpers/mail"
+	"net/http"
+	"crypto/x509"
+	"crypto/tls"
+	"time"
 )
 
 /*
@@ -13,6 +18,58 @@ func SendGmail(FromName, FromEmail, ToName, ToEmail, Subject, Message, PlainMess
 	message := smail.NewSingleEmail(&smail.Email{Name: FromName, Address: FromEmail}, Subject, &smail.Email{Name: ToName, Address: ToEmail}, PlainMessage, Message)
 
 	client := sendgrid.NewSendClient(sendGridAPIKey)
+
+
+	// NEED TO add Godaddy class 2 root ca because GCP does not have this in their f1 vm by default
+	// Get the SystemCertPool, continue with an empty pool on error
+	rootCAs, _ := x509.SystemCertPool()
+	if rootCAs == nil {
+		rootCAs = x509.NewCertPool()
+	}
+	cacert := []byte(`-----BEGIN CERTIFICATE-----
+MIIEADCCAuigAwIBAgIBADANBgkqhkiG9w0BAQUFADBjMQswCQYDVQQGEwJVUzEh
+MB8GA1UEChMYVGhlIEdvIERhZGR5IEdyb3VwLCBJbmMuMTEwLwYDVQQLEyhHbyBE
+YWRkeSBDbGFzcyAyIENlcnRpZmljYXRpb24gQXV0aG9yaXR5MB4XDTA0MDYyOTE3
+MDYyMFoXDTM0MDYyOTE3MDYyMFowYzELMAkGA1UEBhMCVVMxITAfBgNVBAoTGFRo
+ZSBHbyBEYWRkeSBHcm91cCwgSW5jLjExMC8GA1UECxMoR28gRGFkZHkgQ2xhc3Mg
+MiBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eTCCASAwDQYJKoZIhvcNAQEBBQADggEN
+ADCCAQgCggEBAN6d1+pXGEmhW+vXX0iG6r7d/+TvZxz0ZWizV3GgXne77ZtJ6XCA
+PVYYYwhv2vLM0D9/AlQiVBDYsoHUwHU9S3/Hd8M+eKsaA7Ugay9qK7HFiH7Eux6w
+wdhFJ2+qN1j3hybX2C32qRe3H3I2TqYXP2WYktsqbl2i/ojgC95/5Y0V4evLOtXi
+EqITLdiOr18SPaAIBQi2XKVlOARFmR6jYGB0xUGlcmIbYsUfb18aQr4CUWWoriMY
+avx4A6lNf4DD+qta/KFApMoZFv6yyO9ecw3ud72a9nmYvLEHZ6IVDd2gWMZEewo+
+YihfukEHU1jPEX44dMX4/7VpkI+EdOqXG68CAQOjgcAwgb0wHQYDVR0OBBYEFNLE
+sNKR1EwRcbNhyz2h/t2oatTjMIGNBgNVHSMEgYUwgYKAFNLEsNKR1EwRcbNhyz2h
+/t2oatTjoWekZTBjMQswCQYDVQQGEwJVUzEhMB8GA1UEChMYVGhlIEdvIERhZGR5
+IEdyb3VwLCBJbmMuMTEwLwYDVQQLEyhHbyBEYWRkeSBDbGFzcyAyIENlcnRpZmlj
+YXRpb24gQXV0aG9yaXR5ggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQAD
+ggEBADJL87LKPpH8EsahB4yOd6AzBhRckB4Y9wimPQoZ+YeAEW5p5JYXMP80kWNy
+OO7MHAGjHZQopDH2esRU1/blMVgDoszOYtuURXO1v0XJJLXVggKtI3lpjbi2Tc7P
+TMozI+gciKqdi0FuFskg5YmezTvacPd+mSYgFFQlq25zheabIZ0KbIIOqPjCDPoQ
+HmyW74cNxA9hi63ugyuV+I6ShHI56yDqg+2DzZduCLzrTia2cyvk0/ZM/iZx4mER
+dEr/VxqHD3VILs9RaRegAhJhldXRQLIQTO7ErBBDpqWeCtWVYpoNz4iCxTIM5Cuf
+ReYNnyicsbkqWletNw+vHX/bvZ8=
+-----END CERTIFICATE-----`)
+
+	if ok := rootCAs.AppendCertsFromPEM(cacert); !ok {
+		logger.Println("No certs appended, using system certs only")
+	}
+
+
+
+
+	tr := &http.Transport{
+		MaxIdleConns:       -1,
+		IdleConnTimeout:    30 * time.Second,
+		TLSClientConfig: &tls.Config{
+			RootCAs:            rootCAs,
+			},
+		}
+	hc := &http.Client{Transport: tr,
+			Timeout: 10 * time.Second,
+	}
+
+	sendgrid.DefaultClient = &rest.Client{HTTPClient: hc}
 	response, err := client.Send(message)
 	if err != nil {
 		logger.Println(err)
